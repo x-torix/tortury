693b0192103aeab43b1f4159920d6c98568520cd
x-torix <martyna@piwowarska.com.pl>
2023-03-08 15:12:48 +0100

    08.03

--- pub/index.php
+++ pub/index.php
@@ -1,14 +1,25 @@
 <?php
-    require_once('./../src/config.php');
-    use Steampixel\Route;
-    Route::add('/', function(){
-        global $twig;
-        $twig->display("index.html.twig");
-    });
-    Route::add('/upload', function(){
-        global $twig;
-        $twig->display("upload.html.twig");
-    });
-    Route::run('/tortury/pub');
+require_once('./../src/config.php');
 
+use Steampixel\Route;
+
+Route::add('/', function() {
+    global $twig;
+    $twig->display("index.html.twig");
+});
+
+Route::add('/upload', function() {
+    global $twig;
+    $twig->display("upload.html.twig");
+});
+
+Route::add('/upload', function() {
+    global $twig;
+    if(isset($_POST['submit']))  {
+        Post::upload($_FILES['uploadedFile']['tmp_name']);
+    }
+    $twig->display("index.html.twig");
+}, 'post');
+
+Route::run('/cms/pub');
 ?>
