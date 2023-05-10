f6ce41777daaa6816d3854e21df57e49a898b1be
x-torix <martyna@piwowarska.com.pl>
2023-05-10 11:11:30 +0200

    19.04 całe

--- src/Vote.class.php
+++ src/Vote.class.php
@@ -10,27 +10,45 @@ class Vote {
     public static function upVote(int $postId, int $userId) : bool {
         //kod do dodawania upvotów
         global $db;
-        $query = $db->prepare("INSERT INTO vote VALUES (NULL, 1, ?)");
-        $query->bind_param('i', $userId);
+        $query = $db->prepare("INSERT INTO vote VALUES (NULL, ?, 1, ?)");
+        $query->bind_param('ii', $postId, $userId);
         if($query->execute())
             return true;
         return false;
     }
     public static function downVote(int $postId, int $userId) : bool {
         global $db;
-        $query = $db->prepare("INSERT INTO vote VALUES (NULL, -1, ?)");
-        $query->bind_param('i', $userId);
+        $query = $db->prepare("INSERT INTO vote VALUES (NULL, ?, -1, ?)");
+        $query->bind_param('ii', $postId, $userId);
         if($query->execute())
             return true;
         return false;
     }
     public static function getScore(int $postId) : int {
-        //zwróć sumę głosów dla danaego posta
-
-
-        //tymczasowo
+        global $db;
+        //zwróć sumę głosów dla danego posta
+        $query = $db->prepare("SELECT SUM(value) FROM vote WHERE post_id = ?");
+        $query->bind_param('i', $postId);
+        if($query->execute()){
+            $result = $query->get_result();
+            $score = $result->fetch_row()[0];
+            return $score;
+        }
         return 0;
     }
+
+}
+public static function getVote(int $postId, int $userId) : int {
+    //funkcja zwraca (-1,0,1) w zależności od teo czy użytkownik oddał już głos na tego mema
+    global $db;
+    $query = $db->prepare("SELECT value FROM vote WHERE post_id = ? AND user_id = ?");
+    $query->bind_param('ii', $postId, $userId);
+    if($query->execute()) {
+        $vote = $query->get_result()->fetch_row()[0];
+        return $vote;
+    }
+    return 0;
+}
 
 }
 ?>
