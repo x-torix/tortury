e11f4cb4ef9277bba2743a6787fb40e159b56722
x-torix <martyna@piwowarska.com.pl>
2023-05-10 11:08:26 +0200

    19.04 1

--- src/config.php
+++ src/config.php
@@ -3,6 +3,7 @@ require_once('./../vendor/autoload.php');
 $db = new mysqli("localhost", "root", "", "cms");
 require("Post.class.php");
 require("User.class.php");
+require("Vote.class.php");
 $loader = new Twig\Loader\FilesystemLoader("./../src/templates");
 $twig = new Twig\Environment($loader);
 ?>
