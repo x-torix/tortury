9175ff5c28469a4292c24508e7df75ecdee8f074
x-torix <martyna@piwowarska.com.pl>
2023-05-10 11:24:17 +0200

    26.04 2/całość

--- src/Post.class.php
+++ src/Post.class.php
@@ -7,6 +7,7 @@ class Post {
     private string $authorId;
     private string $authorName;
     private int $score;
+    private int $vote;
 
     function __construct(int $i, string $f, string $t, string $title , int $authorId ) {
         $this->id = $i;
@@ -17,6 +18,10 @@ class Post {
         global $db;
         $this->authorName = User::getNameById($this->authorId);
         $this->score = Vote::getScore($this->id);
+        if(User::isAuth())
+            $this->vote = Vote::getVote($this->id, $_SESSION['user']->getId());
+        else 
+            $this->vote = 0;
     }
 
     public function getFilename() : string {
@@ -33,6 +38,9 @@ class Post {
     }
     public function getScore() : int {
         return $this->score;
+    }
+    public function getVote() : int {
+        return $this->vote;
     }
 
     static function getLast() : Post {
